d9dda18141271aad4db3332a18ac585d384d553c
Chlumsky <viktor.chlumsky@gmail.com>
2022-05-08 21:28:28 +0200

    Added spacing between closing angle brackets

--- src/pattern-fill.cpp
+++ src/pattern-fill.cpp
@@ -6,17 +6,26 @@ std::string fillPattern(const std::string &pattern, const Replacer *replacers, i
     str.reserve(2*pattern.size());
     const char *cur = pattern.c_str();
     const char *end = cur+pattern.size();
+    bool closedAngleBracket = false;
     while (cur < end) {
         if (*cur == '$') {
             ++cur;
             for (int i = 0; i < count; ++i) {
                 if (replacers[i].key == *cur) {
                     str += replacers[i].value;
+                    if (!str.empty() && str.back() == '>')
+                        closedAngleBracket = true;
                     break;
                 }
             }
-        } else
+        } else {
+            if (closedAngleBracket) {
+                if (*cur == '>')
+                    str.push_back(' ');
+                closedAngleBracket = false;
+            }
             str.push_back(*cur);
+        }
         ++cur;
     }
     return str;
