da46380162f30a367d3f17138e9658dad2cc37db
Chlumsky <viktor.chlumsky@gmail.com>
2023-03-25 12:32:25 +0100

    Moved const before type

--- src/TypeName.cpp
+++ src/TypeName.cpp
@@ -1,18 +1,24 @@
 
 #include "TypeName.h"
 
+static bool isRefOrPtr(const std::string &nameBody) {
+    return !nameBody.empty() && (nameBody.back() == '&' || nameBody.back() == '*');
+}
+
 std::string TypeName::fullName() const {
     return nameBody+nameSuffix;
 }
 
 std::string TypeName::variableDeclaration(const std::string &variableName) const {
-    return nameBody+' '+variableName+nameSuffix;
+    return nameBody+(isRefOrPtr(nameBody) ? "" : " ")+variableName+nameSuffix;
 }
 
 std::string TypeName::refArgDeclaration(const std::string &argName) const {
-    return nameBody+(nameSuffix.empty() ? " &" : " ")+argName+nameSuffix;
+    return nameBody+(isRefOrPtr(nameBody) ? "" : " ")+(nameSuffix.empty() ? "&" : "")+argName+nameSuffix;
 }
 
 std::string TypeName::constRefArgDeclaration(const std::string &argName) const {
-    return nameBody+(nameSuffix.empty() ? " const &" : " const ")+argName+nameSuffix;
+    if (isRefOrPtr(nameBody))
+        return nameBody+(nameSuffix.empty() ? "const &" : "const ")+argName+nameSuffix;
+    return "const "+nameBody+(nameSuffix.empty() ? " &" : " ")+argName+nameSuffix;
 }
